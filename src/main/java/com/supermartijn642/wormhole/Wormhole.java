a94c1fe32a979063e868db94753d49a4b79f48ac
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-07-27 21:44:44 +0200

    update version

--- src/main/java/com/supermartijn642/wormhole/Wormhole.java
+++ src/main/java/com/supermartijn642/wormhole/Wormhole.java
@@ -19,7 +19,7 @@ public class Wormhole {
 
     public static final String MODID = "wormhole";
     public static final String NAME = "Wormhole (Portals)";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     @GameRegistry.ObjectHolder("wormhole:portal_frame")
     public static Block portal_frame;
