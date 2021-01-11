51628040b38a710879105b3c46318d5e14644ce5
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-11 02:32:21 +0100

    check whether a player is close enough to interact with the gui

--- src/main/java/com/supermartijn642/wormhole/portal/screen/PortalGroupScreen.java
+++ src/main/java/com/supermartijn642/wormhole/portal/screen/PortalGroupScreen.java
@@ -1,9 +1,11 @@
 package com.supermartijn642.wormhole.portal.screen;
 
+import com.supermartijn642.wormhole.ClientProxy;
 import com.supermartijn642.wormhole.portal.PortalGroup;
 import com.supermartijn642.wormhole.portal.PortalGroupTile;
 import com.supermartijn642.wormhole.screen.WormholeScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 
@@ -37,4 +39,16 @@ public abstract class PortalGroupScreen extends WormholeScreen {
         return defaultValue;
     }
 
+    @Override
+    public void tick(){
+        if(!this.canInteractWith(ClientProxy.getPlayer())){
+            this.closeScreen();
+            return;
+        }
+        super.tick();
+    }
+
+    private boolean canInteractWith(PlayerEntity playerIn){
+        return this.pos.distanceSq(playerIn.getPosition()) < 64 * 64;
+    }
 }
