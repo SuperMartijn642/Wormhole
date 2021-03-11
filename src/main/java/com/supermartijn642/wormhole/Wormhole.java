24cce07b6e8b7fc4031f455a8fed88fd3c181ac5
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-03-11 13:18:55 +0100

    update version

--- src/main/java/com/supermartijn642/wormhole/Wormhole.java
+++ src/main/java/com/supermartijn642/wormhole/Wormhole.java
@@ -50,7 +50,7 @@ public class Wormhole {
 
     public static final String MODID = "wormhole";
     public static final String NAME = "Wormhole (Portals)";
-    public static final String VERSION = "1.1.4b";
+    public static final String VERSION = "1.1.5";
     public static final String DEPENDENCIES = "required-after:supermartijn642corelib@[1.0.0,1.1.0)";
 
     @Mod.Instance
