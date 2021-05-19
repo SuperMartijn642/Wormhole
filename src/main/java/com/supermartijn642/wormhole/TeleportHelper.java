0e37009b295c9d74bd6e74118a3e12c0175376dc
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-05-19 17:23:40 +0200

    Properly teleport stacked entities Add a wormhole creative tab

--- src/main/java/com/supermartijn642/wormhole/TeleportHelper.java
+++ src/main/java/com/supermartijn642/wormhole/TeleportHelper.java
@@ -2,8 +2,10 @@ package com.supermartijn642.wormhole;
 
 import com.supermartijn642.wormhole.portal.PortalTarget;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.play.server.SSetPassengersPacket;
 import net.minecraft.util.concurrent.TickDelayedTask;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
@@ -12,6 +14,7 @@ import net.minecraftforge.common.util.ITeleporter;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import java.lang.reflect.Field;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -27,46 +30,63 @@ public class TeleportHelper {
     public static boolean queTeleport(Entity entity, PortalTarget target){
         if(!canTeleport(entity, target))
             return false;
+        for(Entity passenger : entity.getRecursivePassengers())
+            if(passenger instanceof PlayerEntity)
+                return false;
 
-        entity.world.getServer().enqueue(new TickDelayedTask(0, () -> teleportEntity(entity, target)));
-
-        entity.getPersistentData().putLong("wormhole:teleported", entity.ticksExisted);
-        return true;
-    }
-
-    public static boolean teleport(Entity entity, PortalTarget target){
-        if(!canTeleport(entity, target))
-            return false;
-
-        teleportEntity(entity, target);
-
-        entity.getPersistentData().putLong("wormhole:teleported", entity.ticksExisted);
+        Entity lowestEntity = entity.getLowestRidingEntity();
+        if(!entity.world.isRemote){
+            lowestEntity.world.getServer().enqueue(new TickDelayedTask(0, () -> teleportEntityAndPassengers(lowestEntity, null, target)));
+            markEntityAndPassengers(lowestEntity);
+        }
         return true;
     }
 
     public static boolean canTeleport(Entity entity, PortalTarget target){
         if(entity.world.isRemote || !target.getWorld(entity.getServer()).isPresent())
             return false;
+        if(entity.isPassenger())
+            return canTeleport(entity.getLowestRidingEntity(), target);
+
+        for(Entity rider : entity.getRecursivePassengers()){
+            CompoundNBT tag = rider.getPersistentData();
+            if(tag.contains("wormhole:teleported") && rider.ticksExisted - tag.getLong("wormhole:teleported") >= 0 && rider.ticksExisted - tag.getLong("wormhole:teleported") < TELEPORT_COOLDOWN)
+                return false;
+        }
 
         CompoundNBT tag = entity.getPersistentData();
         return !tag.contains("wormhole:teleported") || entity.ticksExisted - tag.getLong("wormhole:teleported") < 0 || entity.ticksExisted - tag.getLong("wormhole:teleported") >= TELEPORT_COOLDOWN;
     }
 
-    private static void teleportEntity(Entity entity, PortalTarget target){
+    private static void markEntityAndPassengers(Entity entity){
+        entity.getPersistentData().putLong("wormhole:teleported", entity.ticksExisted);
+        entity.getPassengers().forEach(TeleportHelper::markEntityAndPassengers);
+    }
+
+    private static void teleportEntityAndPassengers(Entity entity, Entity entityBeingRidden, PortalTarget target){
         if(entity.world.isRemote || !target.getWorld(entity.getServer()).isPresent())
             return;
-
-        Optional<ServerWorld> optionalTargetWorld = target.getWorld(entity.getServer()).filter(ServerWorld.class::isInstance).map(ServerWorld.class::cast);
-        if(!optionalTargetWorld.isPresent())
+        Optional<ServerWorld> targetWorld = target.getWorld(entity.getServer()).filter(ServerWorld.class::isInstance).map(ServerWorld.class::cast);
+        if(!targetWorld.isPresent())
             return;
 
-        ServerWorld targetWorld = optionalTargetWorld.get();
+        Collection<Entity> passengers = entity.getPassengers();
+        entity.removePassengers();
+        Entity newEntity = teleportEntity(entity, targetWorld.get(), target);
+        if(entityBeingRidden != null){
+            newEntity.startRiding(entityBeingRidden);
+            if(newEntity instanceof ServerPlayerEntity)
+                ((ServerPlayerEntity)newEntity).connection.sendPacket(new SSetPassengersPacket(entityBeingRidden));
+        }
+        passengers.forEach(e -> teleportEntityAndPassengers(e, newEntity, target));
+    }
 
+    private static Entity teleportEntity(Entity entity, ServerWorld targetWorld, PortalTarget target){
         if(targetWorld == entity.world){
             if(entity instanceof ServerPlayerEntity)
-                ((ServerPlayerEntity)entity).connection.setPlayerLocation(target.x + .5, target.y + .2, target.z + .5, target.yaw, 0);
+                ((ServerPlayerEntity)entity).teleport(targetWorld, target.x + .5, target.y + .2, target.z + .5, target.yaw, 0);
             else
-                entity.setLocationAndAngles(target.x + .5, target.y + .2, target.z + .5, target.yaw, 0);
+                entity.setPositionAndUpdate(target.x + .5, target.y + .2, target.z + .5);
             entity.setRotationYawHead(target.yaw);
             entity.setMotion(Vector3d.ZERO);
             entity.fallDistance = 0;
@@ -79,9 +99,9 @@ public class TeleportHelper {
                     field_242271_ac.setAccessible(true);
                     field_242271_ac.set(entity, BlockPos.ZERO);
                 }catch(IllegalAccessException e){
-                    return;
+                    return entity;
                 }
-                entity.changeDimension(optionalTargetWorld.get(), new ITeleporter() {
+                Entity newEntity = entity.changeDimension(targetWorld, new ITeleporter() {
                     @Override
                     public Entity placeEntity(Entity entity, ServerWorld currentWorld, ServerWorld destWorld, float yaw, Function<Boolean,Entity> repositionEntity){
                         Entity newEntity = entity.getType().create(targetWorld);
@@ -94,7 +114,10 @@ public class TeleportHelper {
                         return newEntity;
                     }
                 });
+                if(newEntity != null)
+                    return newEntity;
             }
         }
+        return entity;
     }
 }
